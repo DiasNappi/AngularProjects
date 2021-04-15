43411095c5bc7ca5dc42ab24775fe67dba6fdac8
Gabriel <contasnappi@gmail.coml>
2021-04-15 16:04:08 -0300

    Trazendo informacao dinamica para pagina diversao e restaurante

--- src/app/diversao/diversao.component.ts
+++ src/app/diversao/diversao.component.ts
@@ -1,12 +1,21 @@
+import { OfertaService } from "./../ofertas.service";
+import { Oferta } from "./../shared/oferta.model";
 import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-diversao",
   templateUrl: "./diversao.component.html",
   styleUrls: ["./diversao.component.css"],
+  providers: [OfertaService],
 })
 export class DiversaoComponent implements OnInit {
-  constructor() {}
+  public ofertas: Oferta[];
 
-  ngOnInit() {}
+  constructor(public ofertaService: OfertaService) {}
+
+  ngOnInit() {
+    this.ofertaService
+      .getOfertasCategoria("diversao")
+      .then((ofertas: Oferta[]) => (this.ofertas = ofertas));
+  }
 }
--- src/app/restaurantes/restaurantes.component.ts
+++ src/app/restaurantes/restaurantes.component.ts
@@ -1,12 +1,21 @@
+import { OfertaService } from "./../ofertas.service";
+import { Oferta } from "./../shared/oferta.model";
 import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-restaurantes",
   templateUrl: "./restaurantes.component.html",
   styleUrls: ["./restaurantes.component.css"],
+  providers: [OfertaService],
 })
 export class RestaurantesComponent implements OnInit {
-  constructor() {}
+  public ofertas: Oferta[];
 
-  ngOnInit() {}
+  constructor(public ofertaService: OfertaService) {}
+
+  ngOnInit() {
+    this.ofertaService
+      .getOfertasCategoria("restaurante")
+      .then((ofertas: Oferta[]) => (this.ofertas = ofertas));
+  }
 }
